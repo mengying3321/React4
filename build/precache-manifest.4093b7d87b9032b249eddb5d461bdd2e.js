9655e200ce6cef4fca7008be26e1abe6b98a8ff4
2273653338@qq.com <2273653338@qq.com>
2020-11-10 14:47:41 +0800

    淘宝大作业

---
+++ build/precache-manifest.4093b7d87b9032b249eddb5d461bdd2e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d3b26d6b60c691b1d119a79e87e8077",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca8a833976e81918f19c",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "b2643e5e14abfed7e532",
+    "url": "/static/css/main.de91ed64.chunk.css"
+  },
+  {
+    "revision": "ca8a833976e81918f19c",
+    "url": "/static/js/2.437fd88f.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.437fd88f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2643e5e14abfed7e532",
+    "url": "/static/js/main.014d5a21.chunk.js"
+  },
+  {
+    "revision": "345b754ea4504c263d5b3e32280ff2ba",
+    "url": "/static/js/main.014d5a21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
